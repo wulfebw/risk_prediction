b071f0a0cc13cf70223b76ccbaab500b08c1c806
Sou-Cheng Terrya Choi <schoi32@iit.edu>
2017-05-03 15:53:34 -0500

    clean up

--- scripts/utils/util.py
+++ scripts/utils/util.py
@@ -1,15 +1,14 @@
 import os
+import time
 import urllib.request
 
 def download_if_necessary(url, input_filepath, count_max=100):
     if not os.path.exists(input_filepath):
         with urllib.request.urlopen(url) as response, open(input_filepath, 'wb') as out_file:
-            data = response.read()  # a `bytes` object
+            data = response.read()
             out_file.write(data)
 
         count = 1
-        import time
-
         while not os.path.exists(input_filepath) and (count != count_max):
             time.sleep(1)
             count = count + 1
