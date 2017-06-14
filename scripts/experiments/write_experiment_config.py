904445405537495b42afc53da9446aac45c06a0c
wulfebw <wulfebw@stanford.edu>
2017-06-14 16:24:14 -0700

    updating write

--- scripts/experiments/write_experiment_config.py
+++ scripts/experiments/write_experiment_config.py
@@ -1,9 +1,10 @@
 import configparser
 
 # constant accross all
+EXPERIMENT_NAME = 'heuristic_1_lane_5_sec'
 DEFAULTS = {
-    'nprocs': 1,
-    'expdir': '../../data/experiments/test',
+    'nprocs': 24,
+    'expdir': '../../data/experiments/{}'.format(EXPERIMENT_NAME),
     'num_lanes': 1,
     'err_p_a_to_i': .01,
     'err_p_i_to_a': .3,
@@ -30,8 +31,8 @@ def write_collection(config):
     config.set(s, 'col/extract_temporal', 'true')
     config.set(s, 'col/extract_well_behaved', 'true')
     config.set(s, 'col/extract_neighbor', 'true')
-    config.set(s, 'col/extract_behavioral', 'false')
-    config.set(s, 'col/extract_neighbor_behavioral', 'false')
+    config.set(s, 'col/extract_behavioral', 'true')
+    config.set(s, 'col/extract_neighbor_behavioral', 'true')
     config.set(s, 'col/extract_car_lidar', 'true')
     config.set(s, 'col/extract_car_lidar_range_rate', 'true')
     config.set(s, 'col/extract_road_lidar', 'false')
@@ -39,7 +40,7 @@ def write_collection(config):
     # heuristic collection
     config.set(s, 'col/generator_type', 'factored')
     config.set(s, 'col/num_lanes', '%(num_lanes)s')
-    config.set(s, 'col/num_scenarios', '1')
+    config.set(s, 'col/num_scenarios', '2000')
     config.set(s, 'col/num_monte_carlo_runs', '1')
     config.set(s, 'col/err_p_a_to_i', '%(err_p_a_to_i)s')
     config.set(s, 'col/err_p_i_to_a', '%(err_p_i_to_a)s')
@@ -68,16 +69,16 @@ def write_generation(config):
     config.set(s, 'prop_bn_filepath', '%(expdir)s/data/prop_bn.jld')
     config.set(s, 'prop/num_monte_carlo_runs', '1')
     config.set(s, 'prop/prime_time', '0.')
-    config.set(s, 'prop/sampling_time', '1.')
-    config.set(s, 'prop/cem_end_prob', '.5')
-    config.set(s, 'prop/max_iters', '1')
-    config.set(s, 'prop/population_size', '10')
+    config.set(s, 'prop/sampling_time', '5.')
+    config.set(s, 'prop/cem_end_prob', '.25')
+    config.set(s, 'prop/max_iters', '500')
+    config.set(s, 'prop/population_size', '1000')
     config.set(s, 'prop/top_k_fraction', '.5')
     config.set(s, 'prop/n_prior_samples', '50000')
 
     # generation of validation / training data
     ## feature extraction
-    feature_timesteps = 10
+    feature_timesteps = 20
     config.set(s, 'gen/feature_timesteps', '{}'.format(feature_timesteps))
     feature_step_size = 1
     config.set(s, 'gen/feature_step_size', '{}'.format(feature_step_size))
@@ -94,9 +95,9 @@ def write_generation(config):
 
     ## collection with bayes net
     config.set(s, 'gen/generator_type', 'joint')
+    config.set(s, 'gen/num_scenarios', '2000')
+    config.set(s, 'gen/num_monte_carlo_runs', '50')
     config.set(s, 'gen/num_lanes', '%(num_lanes)s')
-    config.set(s, 'gen/num_scenarios', '1')
-    config.set(s, 'gen/num_monte_carlo_runs', '1')
     config.set(s, 'gen/err_p_a_to_i', '%(err_p_a_to_i)s')
     config.set(s, 'gen/err_p_i_to_a', '%(err_p_i_to_a)s')
     config.set(s, 'gen/overall_response_time', '%(overall_response_time)s')
@@ -104,7 +105,7 @@ def write_generation(config):
     config.set(s, 'gen/lat_accel_std_dev', '%(lat_accel_std_dev)s')
     prime_time = (feature_timesteps * feature_step_size) * .1 + .2
     config.set(s, 'gen/prime_time', '{}'.format(prime_time))
-    config.set(s, 'gen/sampling_time', '.1')
+    config.set(s, 'gen/sampling_time', '5.')
     config.set(s, 'gen/max_num_vehicles', '50')
     config.set(s, 'gen/min_num_vehicles', '50')
 
@@ -113,7 +114,9 @@ def write_prediction(config):
     config.add_section(s)
 
     # logistics
-    config.set(s, 'prediction_type', 'batch')
+    config.set(s, 'prediction_type', 'async')
+
+    # async
 
 def write_config(filepath):
     config = configparser.SafeConfigParser(defaults=DEFAULTS)
@@ -124,5 +127,6 @@ def write_config(filepath):
         config.write(outfile)
 
 if __name__ == '__main__':
-    filepath = '../../data/configs/test.cfg'
+    
+    filepath = '../../data/configs/{}.cfg'.format(EXPERIMENT_NAME)
     write_config(filepath)
