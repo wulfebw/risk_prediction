07d56542c7dff37ef126243abaafbb4c5d9b41de
blake wulfe <wulfebw@stanford.edu>
2017-10-20 18:52:58 -0700

    adding ttc to thresholding of ngsim idm parameter estimation

--- scripts/validation/extract_ngsim_idm_parameters.py
+++ scripts/validation/extract_ngsim_idm_parameters.py
@@ -29,9 +29,29 @@ def compute_lengths(arr):
             lengths.append(zero_idxs[0])
     return np.array(lengths)
 
-def remove_collisions_from_x_y(xs, ys, thresh=1.):
+def remove_collisions_from_x_y(
+        xs, 
+        ys, 
+        col_thresh=10., 
+        ttc_thresh=3.):
+    # removes indices where the ttc is below a threshold
     # compares distance between vehicles and the length of the rear vehicle
-    safe_idxs = np.where(xs[:,1] - xs[:,2] > thresh)
+    safe_idxs = []
+    for i, x in enumerate(xs):
+        ds = x[1]
+        # check for near collision
+        if ds < col_thresh:
+            continue
+
+        # check for ttc below threshold (we know ds > 0 at this point)
+        dv = x[3]
+        if dv < 0:
+            ttc = ds / (abs(dv) + 1e-8)
+            if ttc < ttc_thresh:
+                continue
+
+        safe_idxs.append(i)
+
     return xs[safe_idxs], ys[safe_idxs]
 
 def load_simulated_data(
@@ -99,7 +119,7 @@ def load_ngsim_data(
     for i in range(len(idm_features)):
 
         x = idm_features[i,:idm_lengths[i]]
-        y = idm_targets[i, :idm_lengths[i]]
+        y = idm_targets[i,:idm_lengths[i]]
 
         if remove_collisions:
             x, y = remove_collisions_from_x_y(x, y)
@@ -122,18 +142,23 @@ def extract_idm_params_from_x_y(
         v0_bounds,
         s0_bounds,
         T_bounds,
-        b_bounds):
+        b_bounds,
+        min_len=1):
         
-        result = idm_parameter_estimation.estimate_idm_params(
-            x, y,
-            a_bounds=a_bounds,
-            v0_bounds=v0_bounds,
-            s0_bounds=s0_bounds,
-            T_bounds=T_bounds,
-            b_bounds=b_bounds
-        )
+        if len(x) < min_len:
+            return_dict[ego_id] = None
 
-        return_dict[ego_id] = result
+        else:
+            result = idm_parameter_estimation.estimate_idm_params(
+                x, y,
+                a_bounds=a_bounds,
+                v0_bounds=v0_bounds,
+                s0_bounds=s0_bounds,
+                T_bounds=T_bounds,
+                b_bounds=b_bounds
+            )
+
+            return_dict[ego_id] = result
 
 def extract_idm_params(
         xs, 
@@ -141,10 +166,10 @@ def extract_idm_params(
         ids,
         processes=1,
         a_bounds=(.2,6.),
-        v0_bounds=(10.,80.),
-        s0_bounds=(.5,8.),
-        T_bounds=(.5,6.),
-        b_bounds=(.1,9.)):
+        v0_bounds=(5.,90.),
+        s0_bounds=(.2,12.),
+        T_bounds=(.1,6.),
+        b_bounds=(.1,5.)):
     manager = mp.Manager()
     results = manager.dict()
     with mp.Pool(processes=processes) as pool:
@@ -173,7 +198,7 @@ def extract_idm_params(
             sys.stdout.write('\r{} / {} time: {:.5f}'.format(i+1, n_veh, time.time() - st))
             pool_result.get()
 
-    return results
+    return dict(results)
    
 if __name__ == '__main__':
 
@@ -185,17 +210,17 @@ if __name__ == '__main__':
         remove_collisions=True,
         reaction_time_offset=4
     )
-    processes = 2
+    processes = 20
     results = extract_idm_params(xs, ys, ids, processes)
 
     # save the results
     np.save(results_filepath, results)
 
-    losses = np.array([r.fun for (k,r) in results.items()])
+    losses = np.array([r.fun for (k,r) in results.items() if r is not None])
     print('\nmean loss: {}'.format(np.mean(losses)))
 
-    thetas = np.array([r.x for (k,r) in results.items()])
-    idm_parameter_estimation.plot_theta(thetas)
+    # thetas = np.array([r.x for (k,r) in results.items()])
+    # idm_parameter_estimation.plot_theta(thetas)
 
     # # simulated
     # directory = '../../data/datasets/oct/'
